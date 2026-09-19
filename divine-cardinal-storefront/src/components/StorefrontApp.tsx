65a465a6400724bd63cf9feb6376ef9271925d75
Shourya <shourya2510003-d@akgec.ac.in>
2026-09-19 07:46:27 +0000

    fix: include navbar, footer, and global widgets in storefront layout

--- divine-cardinal-storefront/src/components/StorefrontApp.tsx
+++ divine-cardinal-storefront/src/components/StorefrontApp.tsx
@@ -4,6 +4,11 @@ import { CurrencyProvider } from '../context/CurrencyContext';
 import { CartProvider } from '../context/CartContext';
 import { WishlistProvider } from '../context/WishlistContext';
 import HomeClient from './HomeClient';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import ChatbotWidget from './ChatbotWidget';
+import WhatsAppWidget from './WhatsAppWidget';
+import AppPopup from './AppPopup';
 
 interface StorefrontAppProps {
   banners: any[];
@@ -17,7 +22,16 @@ export default function StorefrontApp(props: StorefrontAppProps) {
       <CurrencyProvider>
         <CartProvider>
           <WishlistProvider>
-            <HomeClient {...props} />
+            <div className="flex flex-col min-h-screen">
+              <Navbar />
+              <main className="flex-grow">
+                <HomeClient {...props} />
+              </main>
+              <Footer />
+            </div>
+            <ChatbotWidget />
+            <WhatsAppWidget />
+            <AppPopup />
           </WishlistProvider>
         </CartProvider>
       </CurrencyProvider>
